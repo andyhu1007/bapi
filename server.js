bc42912969b5b8376e6a06f3195393d5ebcc690d
andy <andyhu1007@gmail.com>
2010-11-10 14:26:46 +0800

    andy - #N/A - refactor server

--- server.js
+++ server.js
@@ -1,49 +1,53 @@
-var CONTENT_TYPE = {
-  js : 'text/plain',
-  html : 'text/html',
-  mp3 : 'audio/mpeg'
+var server = function(hostname, port) {
+    this.start(hostname, port);    
 }
 
-var render = function(file, res){
-  var fs = require('fs');
-  fs.readFile(file, function (err, data) {
-    console.log("rendering " + file);
-    if (err) {
-      handleError(res, err);
-    }else {
-      handle(res, file, data)
-    }    
-  });  
+server.prototype.CONTENT_TYPE = {
+    js : 'text/plain',
+    html : 'text/html',
+    mp3 : 'audio/mpeg'
+}
+
+server.prototype.render = function(file, res) {
+    var self = this;
+    require('fs').readFile(file, function (err, data) {
+        console.log("rendering " + file);
+        if (err) {
+            self.handleError(res, err);
+        } else {
+            self.handle(res, file, data)
+        }
+    });
 };
 
-var handle = function(res, file, data) {
-  var fileSuffix = file.match(/.+\.(\w+)/)[1];
-  res.writeHead(200, {'Content-Type': eval("CONTENT_TYPE." + fileSuffix), 'Content-Length' : data.length}); 
-  res.end(data);
+server.prototype.handle = function(res, file, data) {
+    var fileSuffix = file.match(/.+\.(\w+)/)[1];
+    res.writeHead(200, {'Content-Type': eval("this.CONTENT_TYPE." + fileSuffix), 'Content-Length' : data.length});
+    res.end(data);
 }
 
-var handleError = function(res, err) {  
-  if(err.message.indexOf('No such file or directory') >= 0) {
-    res.writeHead(404, {'Content-Type': 'text/plain'}); 
-    res.end('No page found!');
-  }else {
-    res.writeHead(500, {'Content-Type': 'text/plain'}); 
-    res.end('Server error!');
-  }
-  console.log(err.message);
+server.prototype.handleError = function(res, err) {
+    if (err.message.indexOf('No such file or directory') >= 0) {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('No page found!');
+    } else {
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Server error!');
+    }
+    console.log(err.message);
 }
 
-var parse = function(url) {
-  var pathname = require('url').parse(url).pathname
-  return '/' == pathname ? 'home.html' : pathname.match(/\/(.+)/)[1]
+server.prototype.mapToFile = function(url) {
+    var pathname = require('url').parse(url).pathname
+    return '/' == pathname ? 'home.html' : pathname.match(/\/(.+)/)[1]
 }
 
-var startServer = function(hostname, port) {
-  var http = require('http');
-  http.createServer(function (req, res) {   
-    render(parse(req.url), res);
-  }).listen(port, hostname);
-  console.log('Server running at http://' + hostname + ':' + port + '/');
+server.prototype.start = function(hostname, port) {
+    var self = this;
+    require('http').createServer(function (req, res) {
+        self.render(self.mapToFile(req.url), res);
+    }).listen(port, hostname);
+    console.log('Server running at http://' + hostname + ':' + port + '/');
 };
 
-startServer('127.0.0.1', '8124');
+new server('127.0.0.1', '8124');
