478a5743291053bc19d5b4856de024f3a8e9adf7
andy <andyhu1007@gmail.com>
2010-11-10 15:34:23 +0800

    andy - #N/A - refactor server

--- http_server.js
+++ http_server.js
@@ -1,19 +1,19 @@
-var server = function(hostname, port) {
+var httpServer = function(hostname, port) {
     this.start(hostname, port);
 }
 
-server.prototype.CONTENT_TYPE = {
+httpServer.prototype.CONTENT_TYPE = {
     PLAIN : 'text/plain',
     HTML : 'text/html',
     MPEG : 'audio/mpeg'
 }
 
-server.prototype.STATUS_CODE = {
+httpServer.prototype.STATUS_CODE = {
     NOT_FOUND : 404,
-    SERVER_ERROR : 500
+    httpServer_ERROR : 500
 }
 
-server.prototype._contentType = function(file) {
+httpServer.prototype._contentType = function(file) {
     var CONTENT_TYPES = {
         js : this.CONTENT_TYPE.PLAIN,
         html : this.CONTENT_TYPE.HTML,
@@ -23,15 +23,15 @@ server.prototype._contentType = function(file) {
     return eval("CONTENT_TYPES." + fileSuffix);
 }
 
-server.prototype._statusCode = function(err) {
+httpServer.prototype._statusCode = function(err) {
     var fileNotFound = err.message.indexOf('No such file or directory') >= 0;
     return {
-        code: fileNotFound ? this.STATUS_CODE.NOT_FOUND : this.STATUS_CODE.SERVER_ERROR,
-        msg: fileNotFound ? 'No page found!' : 'Server error!'
+        code: fileNotFound ? this.STATUS_CODE.NOT_FOUND : this.STATUS_CODE.httpServer_ERROR,
+        msg: fileNotFound ? 'No page found!' : 'httpServer error!'
     }
 }
 
-server.prototype._render = function(file, res) {
+httpServer.prototype._render = function(file, res) {
     var self = this;
     require('fs').readFile(file, function (err, data) {
         console.log("get the request for " + file);
@@ -43,29 +43,29 @@ server.prototype._render = function(file, res) {
     });
 };
 
-server.prototype._response = function(res, file, data) {
+httpServer.prototype._response = function(res, file, data) {
     res.writeHead(200, {'Content-Type': this._contentType(file), 'Content-Length' : data.length});
     res.end(data);
 }
 
-server.prototype._error = function(res, err) {
+httpServer.prototype._error = function(res, err) {
     var status = this._statusCode(err);
     res.writeHead(status.code, {'Content-Type': this.CONTENT_TYPE.PLAIN});
     res.end(status.msg);
     console.log(err);
 }
 
-server.prototype._pathToFile = function(url) {
+httpServer.prototype._pathToFile = function(url) {
     var pathname = require('url').parse(url).pathname
     return '/' == pathname ? 'home.html' : pathname.match(/\/(.+)/)[1]
 }
 
-server.prototype.start = function(hostname, port) {
+httpServer.prototype.start = function(hostname, port) {
     var self = this;
     require('http').createServer(function (req, res) {
         self._render(self._pathToFile(req.url), res);
     }).listen(port, hostname);
-    console.log('Server running at http://' + hostname + ':' + port + '/');
+    console.log('httpServer running at http://' + hostname + ':' + port + '/');
 };
 
-new server('127.0.0.1', '8124');
+new httpServer('127.0.0.1', '8124');
